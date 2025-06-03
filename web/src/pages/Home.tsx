3880abf391a12e791fc97da77215ac3cf01426d4
mateus-cristian <mateus_cristian@outlook.com>
2025-06-03 19:22:33 -0300

    feat: separating back and front end

---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Banner from '../components/banner'
+import Header from '../components/header'
+import Products from '../components/products'
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <Products />
+        </>
+    )
+}
