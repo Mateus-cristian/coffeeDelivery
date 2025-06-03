293d48f995552b7cc30a589312654294e27828b5
mateus-cristian <mateus_cristian@outlook.com>
2025-06-02 21:06:44 -0300

    fix: Position cards and grid

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -31,16 +31,18 @@ export default function Products() {
 
 
     return (
-        <div className='mt-32'>
+        <div className='mt-32 flex flex-col align-start'>
             <h2 className='text-subtitle text-[2rem] font-title font-extrabold'>Nossos cafés</h2>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-20 place-items-center pb-20'>
-                {coffes.map((coffee: CoffeeProps, index: number) =>
+            <div className='grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-20 place-items-center pb-20 xxl:place-items-start xxl:grid-cols-[repeat(auto-fit,minmax(288px,1fr))] '>
+                {coffes.map((coffee: CoffeeProps) =>
                     <div key={coffee.id} className='relative rounded-md rounded-tr-[2rem] rounded-bl-[2rem] bg-alt min-h-[310px] max-w-[256px]'>
                         <img src={coffee.image} alt="" className='mx-auto translate-y-[-20px]' />
                         <div className='flex flex-col px-5 pb-5'>
                             <div className='flex justify-center gap-1'>
-                                {coffee.variation.map((item) => (
-                                    <span key={index} className='inline text-center py-1 px-2 text-yellowDark bg-yellowLight rounded-[30px] max-h-5 max-w-20 text-[0.625rem] font-bold uppercase translate-y-[-10px]'>{item.name}</span>
+                                {coffee.variation.map((item, index) => (
+                                    <span key={index} className='inline text-center py-1 px-2 text-yellowDark bg-yellowLight rounded-[30px] max-h-5 max-w-20 text-[0.625rem] font-bold uppercase translate-y-[-10px]'>
+                                        {item.name}
+                                    </span>
                                 ))}
                             </div>
                             <span className='text-center text-title font-title text-xl font-bold '>{coffee.name}</span>
